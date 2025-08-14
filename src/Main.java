4f3c08949800b498cbcd953604e1570f526f6cd5
eimano <eimano.hassan18@gmail.com>
2025-08-14 16:17:17 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,9 @@
-import java.awt.Dimension;
-import java.awt.Graphics;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Main extends JFrame {
+public class Main {
     public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+        System.out.println("Red vs. Blue");
     }
 
-    class Canvas extends JPanel {
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-      }
+    
 
-      @Override
-      public void paint(Graphics g) {
-	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
-      }
-    }
 
-    private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
-    }
-
-    public void run() {
-      while(true) {
-        repaint();
-      }
-    }
 }
