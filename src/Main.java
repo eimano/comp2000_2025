a7de2e89c14ddb48809d72f2f8b29d82c30c4460
eimano <eimano.hassan18@gmail.com>
2025-09-18 09:08:11 +1000

    I have completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
 
-public class Main {
+
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
+Main window = new Main();
+      
+    }
+
+class Canvas extends JPanel {
+    public Canvas() {
+  setPreferredSize(new Dimension( 720, 720));
 
     }
+}
+
+@Override
+public void paint(Graphics g){
+    for(int i = 20; i < 710; i+=35){
+        for(int j = 45; j <= 710; j+=35){
+            g.setColor(Color.BLACK);
+            g.fillRect(i, j, 35, 35);
+            g.setColor(Color.WHITE);
+            g.drawRect(i, j, 35, 35);
+        }
+    }
+
+}
+
+ private Main() {
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Canvas canvas = new Canvas();
+      this.setContentPane(canvas);
+      this.pack();
+      this.setVisible(true);
+ }
 }
